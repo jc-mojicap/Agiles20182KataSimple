6cf2da71c1221cfd12e7f6cb54978d5ad1cfda60
JDTorresP <jd.torres11@uniandes.edu.co>
2018-09-30 19:57:35 -0500

    Paso 1: Se retorna [0,0,0] para string vacio (VERDE)

--- Estadisticas.py
+++ Estadisticas.py
@@ -1,9 +1,4 @@
 class Estadistica:
 
-    def calcular(self, cadena):
-        arreglo = cadena.split(",")
-        if len(cadena) == 0:
-            return [0, 0]
-        else:
-            minimo = int(min(arreglo))
-            return [len(arreglo), minimo]
+    def calcular(cadena):
+        return [0, 0, 0]
