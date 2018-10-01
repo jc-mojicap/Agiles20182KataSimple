e1bf641fb508f322d3bf51117053433487592511
JDTorresP <jd.torres11@uniandes.edu.co>
2018-09-30 20:09:18 -0500

    Paso 3: Prueba de string con dos elementos (ROJO)

--- Testcases.py
+++ Testcases.py
@@ -10,3 +10,7 @@ class Test(TestCase):
 
     def test_stringUnElemento(self):
         self.assertEqual(Estadistica().calcular("1"), [1, 1, 1, 1], "String con un elemento")
+
+    def test_stringDosElementos(self):
+        self.assertEqual(Estadistica().calcular("1,2"), [2, 1, 2, 1.5], "String con dos elemento")
+        
