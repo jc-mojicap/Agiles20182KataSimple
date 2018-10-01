c15834935d71c0445f0c3dd091a0665979dbc8f3
jc-mojicap <jc.mojicap@uniandes.edu.co>
2018-09-30 19:53:18 -0500

    Paso 4: Se retorna [0,0] para String vacio, y el número de elementos y el minimo para otros casos (VERDE)

--- Estadisticas.py
+++ Estadisticas.py
@@ -1,9 +1,9 @@
 class Estadistica:
 
     def calcular(self, cadena):
+        arreglo = cadena.split(",")
         if len(cadena) == 0:
             return [0, 0]
-        elif len(cadena) == 1:
-            return [1, 1]
         else:
-            return [2, 1]
+            minimo = int(min(arreglo))
+            return [len(arreglo), minimo]
