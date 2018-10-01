4902257f55482f92b70f25c3c3c9607a19451e9f
JDTorresP <jd.torres11@uniandes.edu.co>
2018-09-30 19:46:24 -0500

    Paso 2: Prueba de string con un elemento (ROJO)

--- Testcases.py
+++ Testcases.py
@@ -7,3 +7,7 @@ class Test(TestCase):
 
     def test_stringVacio(self):
         self.assertEqual(Estadistica().calcular(""), [0, 0], "String vacio")
+
+    def test_stringUnElemento(self):
+        self.assertEqual(Estadistica().calcular("1"), [1, 1], "String con un elemento")
+        
